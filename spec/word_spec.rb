3917b4058432f481f6098f69184b099efbacfc05
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-06 07:23:18 -0700

    Test for saving a word passed.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -7,5 +7,23 @@ describe '#Word' do
       expect(Word.all).to(eq([]))
     end
   end
+
+  describe ('#save') do 
+    it("saves a word") do
+      word = Word.new("apple", nil)
+      word.save()
+      word2 = Word.new("yellow", nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
+
+  describe('#==') do
+   it("is the same word if it has the same attributes as another word") do
+      word = Word.new("apple", nil)
+      word2 = Word.new("apple", nil)
+      expect(word).to(eq(word2))
+   end
+  end
 end
 
