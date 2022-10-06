5ceabdda101c4776d548c035fdd78e3931fae0ca
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-06 08:50:58 -0700

    All tests for definitions passed.

--- app.rb
+++ app.rb
@@ -1,5 +1,53 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/album')
+require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  name = params[:word_name]
+  word = Word.new(name, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
+
+get('/words/:id') do
+  @word =Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+  if params[:name] != ""
+    @word = Word.find(params[:id].to_i())
+    @word.update(params[:name])
+  end
+  @words = Word.all
+  erb(:words)
+end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
