b1212de2d5cb225d3c78fd8a9449e337b3a5dd7b
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-09 10:50:45 -0700

    Added capybara and integration spec. Test passed.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -12,3 +12,14 @@ describe('create an word path', {:type => :feature}) do
     expect(page).to have_content('yellow')
   end
 end
+
+describe('create a definition path', {:type => :feature}) do
+  it('creates a definition and then goes to the word page') do
+    word = Word.new("yellow", nil)
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('definition_name', :with => 'a primary color')
+    click_on('Add definition')
+    expect(page).to have_content('a primary color')
+  end
+end
