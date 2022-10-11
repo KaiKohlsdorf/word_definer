842c48da272513193908f6bb6ce04e71e1c1795b
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-10 19:19:30 -0700

    Found some bugs for definitions. WIP.

--- app.rb
+++ app.rb
@@ -50,28 +50,28 @@ delete('/words/:id') do
 end
 
 get('/words/:id/definitions/:definition_id') do
-  @definition = Definition.find(params[:song_id].to_i())
+  @definition = Definition.find(params[:definition_id].to_i())
   erb(:definition)
 end
 
 post('/words/:id/definitions') do
   @word = Word.find(params[:id].to_i())
-  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition = Definition.new(params[:definition_definition], @word.id, nil)
   definition.save()
   erb(:word)
 end
 
 patch('/words/:id/definitions/:definition_id') do
   @word = Word.find(params[:id].to_i())
-  if params[:name] != ""
+  if params[:definition] != ""
     definition = Definition.find(params[:definition_id].to_i())
-    definition.update(params[:name], @word.id)
+    definition.update(params[:definition], @word.id)
   end
   erb(:word)
 end
 
 delete('/words/:id/definitions/:definition_id') do
-  @definition = Definition.find(params[:song_id].to_i())
+  definition = Definition.find(params[:song_id].to_i())
   definition.delete
   @word = Word.find(params[:id].to_i())
   erb(:word)
