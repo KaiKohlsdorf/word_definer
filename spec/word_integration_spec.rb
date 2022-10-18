19baffadc2daba76cfda9fa2d595d65b1c0a6aa4
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-18 00:10:49 -0700

    Update word integration test passed.

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,7 +3,7 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('create an word path', {:type => :feature}) do
+describe('create a word path', {:type => :feature}) do
   it('creates a word and then goes to the word page') do
     visit('/words')
     click_on('Add a new word')
@@ -23,3 +23,26 @@ describe('create a definition path', {:type => :feature}) do
     expect(page).to have_content('a primary color')
   end
 end
+
+describe('update a word', {:type => :feature}) do
+  it('updates a word and then goes to the word page') do
+    word = Word.new("yellow", nil)
+    word.save
+    visit("/words/#{word.id}")
+    click_on('Edit word')
+    fill_in('name', :with => 'blue')
+    click_on('Update')
+    expect(page).to have_content('blue')
+  end
+end
+
+# describe('delete a word', {:type => :feature}) do
+#   it('deletes a word and then goes to the word page') do
+#     word = Word.new("yellow", nil)
+#     word.save
+#     visit("/words/#{word.id}")
+#     click_on('Edit word')
+#     click_on('Delete word')
+#     expect(page).to have_content('')
+#   end
+# end
